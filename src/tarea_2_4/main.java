30c1e2e3666dfb09d1fa545a42718c28f7551195
Khalifa Boulbayem (ext) <khalifa.boulbayem.external@eviden.com>
2024-03-19 17:44:42 +0000

    Crear clase main inicial

---
+++ src/tarea_2_4/main.java
@@ -0,0 +1,16 @@
+package tarea_2_4;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO 
+		//1 crear una instancia de un objeto.
+		
+		//2 crear una instancia de un objeto con la dependencia de inyeccion
+		
+		
+		//Mostrar la informacion por la consola.
+
+	}
+
+}
